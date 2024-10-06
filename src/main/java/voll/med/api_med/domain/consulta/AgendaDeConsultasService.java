e1125383fa116ede5571e1cde5b598836d0b58a8
Leonardo Lazilha <107946409+LeonardoLazilha@users.noreply.github.com>
2024-10-06 20:48:17 -0300

    feat(agendamento-consulta): adicionar validações de integridade para paciente e médico no agendamento de consulta

--- src/main/java/voll/med/api_med/domain/consulta/AgendaDeConsultasService.java
+++ src/main/java/voll/med/api_med/domain/consulta/AgendaDeConsultasService.java
@@ -2,6 +2,7 @@ package voll.med.api_med.domain.consulta;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import voll.med.api_med.domain.ValidacaoException;
 import voll.med.api_med.domain.consulta.dto.AgendamentoConsultaDTO;
 import voll.med.api_med.domain.medico.MedicoRepository;
 import voll.med.api_med.domain.paciente.PacienteRepository;
@@ -10,16 +11,24 @@ import voll.med.api_med.domain.paciente.PacienteRepository;
 @RequiredArgsConstructor
 public class AgendaDeConsultasService {
 
-    private ConsultaRepository consultaRepository;
-    private MedicoRepository medicoRepository;
-    private PacienteRepository pacienteRepository;
+    private final ConsultaRepository consultaRepository;
+    private final MedicoRepository medicoRepository;
+    private final PacienteRepository pacienteRepository;
 
 
-    public void agendar(AgendamentoConsultaDTO dados){
+    public void agendar(AgendamentoConsultaDTO dados)  {
+        if (!pacienteRepository.existsById(dados.idPaciente())){
+        throw new ValidacaoException("Id do paciente não localizado.");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+        throw new ValidacaoException("Id do médico não localizado.");
+        }
 
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var medico = medicoRepository.findById(dados.idMedico()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
+
         consultaRepository.save(consulta);
     }
 }

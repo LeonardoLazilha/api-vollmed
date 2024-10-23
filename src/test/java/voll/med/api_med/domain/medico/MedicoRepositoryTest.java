7302f291d0aa50a257836fd60b9df0d164efb8c3
Leonardo Lazilha <107946409+LeonardoLazilha@users.noreply.github.com>
2024-10-23 08:33:16 -0300

    tentativa de resolucao testes

--- src/test/java/voll/med/api_med/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/voll/med/api_med/domain/medico/MedicoRepositoryTest.java
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class MedicoRepositoryTest {
-
-    @Autowired
-    private MedicoRepository repository;
+class xMedicoRepositoryTest {
 
     @Test
     void escolherMedicoAleatorioLivreNaData() {
